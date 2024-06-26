f6caf2272c6541ba8308eb93086d7303bd3fe71e
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-06-26 23:03:02 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','6426072097:AAFpvNEzW65qVT6XwTxzafDLJKFk-Xtbun0')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
