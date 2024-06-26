779b68b025cbe840b78a59e7132724fd1458c174
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-06-26 23:02:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -13,9 +13,9 @@ from helper.date import add_date ,check_expi
 CHANNEL = os.environ.get('CHANNEL',"")
 import datetime
 from datetime import date as date_
-STRING = os.environ.get("STRING","")
-log_channel = int(os.environ.get("LOG_CHANNEL",""))
-token = os.environ.get('TOKEN','')
+STRING = os.environ.get("STRING","1BVtsOJ0BuxU8Byssa_oQycEq_GuZzedIecoJeuolkotKQiDAETgZtqcn4a9b8caaeP-I2O2guVbw5YEPIuR7QJTAzz5NqJCF_MIG2L61ZMfJFKwDzTUGNdRcRsUECEvgrGKvDG7-PBOzd5IrfGyV2qRS5WFG-KQMz86BMjkSsjPkzg8IW4jbz_ltQuKUsZj2tMxxkbBlgik_DJcOHDGhWZSCj4Kiq4CTz1b0riNEjahBCxphxSmPOC5J6QcVjpj-SH5ArMcimxfvpLDjlM270ct1w66oU2O2zN1ITrnI3BJzlx8jA1c3NCiuV7jTg8KvOuatv6yT7-pynJXxaOQdRBRqEtg_Cx0=")
+log_channel = int(os.environ.get("LOG_CHANNEL","-1002049486679"))
+token = os.environ.get('TOKEN','6426072097:AAFpvNEzW65qVT6XwTxzafDLJKFk-Xtbun0')
 botid = token.split(':')[0]
 
 #Part of Day --------------------
